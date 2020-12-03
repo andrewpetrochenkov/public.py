246b182e4b9200b69ab0de66b1dcfa22dc8ed1b0
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:14:44 +0300

    2020:12:04 00:14

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='public',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
